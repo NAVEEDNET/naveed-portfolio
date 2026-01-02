c0ea4ec21ee2ffc95077db409bf6e7438fe65ca5
NAVEEDNET <93482467+NAVEEDNET@users.noreply.github.com>
2026-01-02 22:45:50 +0530

    solve error on about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -58,7 +58,7 @@ const About = () => {
       </div>
 
       {/* Tailwind custom animations */}
-      <style jsx>{`
+      <style>{`
         @keyframes fadeInUp {
           0% { opacity: 0; transform: translateY(20px); }
           100% { opacity: 1; transform: translateY(0); }
